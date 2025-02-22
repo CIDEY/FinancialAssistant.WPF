da1758db18ecd545d9d3e6053a46beb710c9b955
CIDEY <421ispp.ktits#>
2025-02-23 01:03:28 +0300

    add Auth

---
+++ FinancialAssistant/Classes/ThemeManager.cs
@@ -0,0 +1,24 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialAssistant.Classes
+{
+    public static class ThemeManager
+    {
+        private static readonly PaletteHelper _paletteHelper = new PaletteHelper();
+
+        public static void SetTheme(bool isDark)
+        {
+            ITheme theme = _paletteHelper.GetTheme();
+            IBaseTheme baseTheme = isDark
+                ? new MaterialDesignDarkTheme()
+                : (IBaseTheme)new MaterialDesignLightTheme();
+            theme.SetBaseTheme(baseTheme);
+            _paletteHelper.SetTheme(theme);
+        }
+    }
+}
